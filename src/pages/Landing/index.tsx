5b14c8fe7d7ff785cfdf3c4df54b7b77e2aa7a49
Henrique Lopes <henriquegamer83@gmail.com>
2020-08-06 21:32:34 -0300

    StackNavigation created

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { View, Image, Text, TouchableOpacity } from "react-native";
+import { View, Image, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from "./styles";
 
@@ -9,6 +11,12 @@ import giveClassesIcon from "../../assets/images/icons/give-classes.png";
 import heartIcon from "../../assets/images/icons/heart.png";
 
 function Landing() {
+  const navigation = useNavigation();
+
+  function handleNavigationToGiveClassesPage() {
+    navigation.navigate("GiveClasses");
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner} />
@@ -18,18 +26,20 @@ function Landing() {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+        <RectButton style={[styles.button, styles.buttonPrimary]}>
           <Image source={studyIcon} />
           <Text style={styles.buttonText}>Estudar</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+        </RectButton>
+        <RectButton
+          onPress={handleNavigationToGiveClassesPage}
+          style={[styles.button, styles.buttonSecondary]}
+        >
           <Image source={giveClassesIcon} />
           <Text style={styles.buttonText}>Dar aulas</Text>
-        </TouchableOpacity>
+        </RectButton>
       </View>
       <Text style={styles.totalConnections}>
-        Total de 10 conexoes ja realizadas {' '}
-        <Image source={heartIcon} />
+        Total de 10 conexoes ja realizadas <Image source={heartIcon} />
       </Text>
     </View>
   );
--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import Landing from '../pages/Landing';
+import Landing from "../pages/Landing";
+import GiveClasses from "../pages/GiveClasses";
 
 const { Navigator, Screen } = createStackNavigator();
 
 function AppStack() {
-    return (
-        <NavigationContainer>
-            <Navigator>
-                <Screen name="Landing" component={Landing} />
-            </Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="Landing" component={Landing} />
+        <Screen name="GiveClasses" component={GiveClasses} />
+      </Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default AppStack;
